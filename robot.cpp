c9bb57168da6713d3ff61b16462f2c57d972e545
Nawaf Abdullah <nawaf97k@gmail.com>
2017-11-06 17:19:38 -0500

    defined Robot functions

--- robot.cpp
+++ robot.cpp
@@ -1,7 +1,16 @@
 #include "robot.h"
+#include <iostream>
 
-Robot::Robot()
-{
+Robot::Robot(){}
 
+Robot::setPower(bool pwr){
+    power = pwr;
+}
+
+Robot::selfDestruct(){
+    numRobots--;
+    cout << "Robot destroyed!" << endl;
+    cout << "Number of robots: " << numRobots << endl;
+    delete this;
 }
 
